87b6d5aefe3ce3b6e5ce5701e19cc56db8b2011e
LeoKnox <LeoKnox@users.noreply.github.com>
2020-02-17 13:59:58 -0700

    Add files via upload

---
+++ Data/MvcCharacterContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using MvcCharacter.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCharacter.Data
+{
+    public class MvcCharacterContext : DbContext
+    {
+        public MvcCharacterContext (DbContextOptions<MvcCharacterContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Character> Character { get; set; }
+    }
+}
---
+++ Models/Character.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCharacter.Models
+{
+    public class Character
+    {
+        public int CharacterId { get; set; }
+        public string CharacterName { get; set; }
+        public string CharacterClass { get; set; }
+        public int HP { get; set; }
+        public int AC { get; set; }
+    }
+}
