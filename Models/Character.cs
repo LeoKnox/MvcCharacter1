c2bcc6e4a852712aadf0be537071a9c6a9b98ca0
LeoKnox <LeoKnox@users.noreply.github.com>
2020-02-17 14:46:07 -0700

    Add files via upload

--- Models/Character.cs
+++ Models/Character.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,9 +10,15 @@ namespace MvcCharacter.Models
     public class Character
     {
         public int CharacterId { get; set; }
+        [Required]
+        [Display(Name = "Name")]
         public string CharacterName { get; set; }
+        [Required]
+        [Display(Name = "Class")]
         public string CharacterClass { get; set; }
+        [Range(1, Int32.MaxValue)]
         public int HP { get; set; }
+        [Range(1, 100)]
         public int AC { get; set; }
     }
 }
